af6b3865cc01962fe48f698f6e19ce13f17f9054
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-02-18 21:33:24 -0800

    Compile React source files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = (env, argv) => {
   const is_dev = argv.mode === 'development';
   return {
     devtool: is_dev ? 'source-map' : 'none',
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
     output: {
       filename: 'bundle.js',
       path: path.join(__dirname, './public/js' + (is_dev ? '-dev' : ''))
