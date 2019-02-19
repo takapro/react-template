796dfd4e9467faa97cfd141386678869ffc9b276
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-02-18 16:42:30 -0800

    Compile TypeScript source file

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,18 @@ module.exports = (env, argv) => {
   const is_dev = argv.mode === 'development';
   return {
     devtool: is_dev ? 'source-map' : 'none',
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
       filename: 'bundle.js',
       path: path.join(__dirname, './public/js' + (is_dev ? '-dev' : ''))
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, use: [{ loader: 'ts-loader' }] }
+      ]
     }
   };
 };
